9897830f63c6158deca2faa7bc3ff31bde85ea77
Idan Koren-Israeli <idan503@github.com>
2020-12-30 15:18:21 +0200

    Added a simple qwerty layout

--- app/src/main/java/com/idankorenisraeli/spyboard/MyInputMethodService.java
+++ app/src/main/java/com/idankorenisraeli/spyboard/MyInputMethodService.java
@@ -4,61 +4,86 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
 
-public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+public class MyInputMethodService extends InputMethodService{
 
     @Override
     public View onCreateInputView() {
         // get the KeyboardView and add our Keyboard layout to it
         KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
-        Keyboard keyboard = new Keyboard(this, R.xml.number_pad);
+        Keyboard keyboard = new Keyboard(this, R.xml.keyboard_qwerty);
         keyboardView.setKeyboard(keyboard);
-        keyboardView.setOnKeyboardActionListener(this);
+        keyboardView.setOnKeyboardActionListener(keyBoardAction);
         return keyboardView;
     }
 
-    @Override
-    public void onKey(int primaryCode, int[] keyCodes) {
-
-        InputConnection ic = getCurrentInputConnection();
-        if (ic == null) return;
-        switch (primaryCode) {
-            case Keyboard.KEYCODE_DELETE:
-                CharSequence selectedText = ic.getSelectedText(0);
-                if (TextUtils.isEmpty(selectedText)) {
-                    // no selection, so delete previous character
-                    ic.deleteSurroundingText(1, 0);
-                } else {
-                    // delete the selection
-                    ic.commitText("", 1);
-                }
-                break;
-            default:
-                char code = (char) primaryCode;
-                ic.commitText(String.valueOf(code), 1);
+    private void pressedDelete(InputConnection ic){
+        CharSequence selectedText = ic.getSelectedText(0);
+        if (TextUtils.isEmpty(selectedText)) {
+            // no selection, so delete previous character
+            ic.deleteSurroundingText(1, 0);
+        } else {
+            // delete the selection
+            ic.commitText("", 1);
         }
     }
 
-    @Override
-    public void onPress(int primaryCode) { }
+    KeyboardView.OnKeyboardActionListener keyBoardAction = new KeyboardView.OnKeyboardActionListener() {
+        @Override
+        public void onPress(int primaryCode) {
 
-    @Override
-    public void onRelease(int primaryCode) { }
+        }
 
-    @Override
-    public void onText(CharSequence text) { }
+        @Override
+        public void onRelease(int primaryCode) {
 
-    @Override
-    public void swipeLeft() { }
+        }
 
-    @Override
-    public void swipeRight() { }
+        @Override
+        public void onKey(int primaryCode, int[] keyCodes) {
+            Log.i("pttt", "primary: " + primaryCode + " | keycodes: " + keyCodes.length);
+            InputConnection ic = getCurrentInputConnection();
+            if (ic == null) return;
+            switch (primaryCode) {
+                case Keyboard.KEYCODE_DELETE:
+                    pressedDelete(ic);
+                    break;
+                case Keyboard.KEYCODE_MODE_CHANGE:
+                    //change to hebrew
+                    break;
+                default:
+                    char code = (char) primaryCode;
+                    ic.commitText(String.valueOf(code), 1);
+            }
+        }
 
-    @Override
-    public void swipeDown() { }
 
-    @Override
-    public void swipeUp() { }
+        @Override
+        public void onText(CharSequence text) {
+
+        }
+
+        @Override
+        public void swipeLeft() {
+
+        }
+
+        @Override
+        public void swipeRight() {
+
+        }
+
+        @Override
+        public void swipeDown() {
+
+        }
+
+        @Override
+        public void swipeUp() {
+
+        }
+    };
 }
