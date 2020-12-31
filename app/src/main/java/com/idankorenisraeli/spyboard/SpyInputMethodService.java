451cc18cb18de62dd786433bc6d661aa5eef2cd3
Idan Koren-Israeli <idan503@github.com>
2020-12-31 09:08:22 +0200

    Full hebrew keyboard support

--- app/src/main/java/com/idankorenisraeli/spyboard/SpyInputMethodService.java
+++ app/src/main/java/com/idankorenisraeli/spyboard/SpyInputMethodService.java
@@ -69,11 +69,87 @@ public class SpyInputMethodService extends android.inputmethodservice.InputMetho
     
 
     private void pressedCharacter(int primaryCode, InputConnection ic){
-        primaryCode = caps ? primaryCode + ('A' - 'a') : primaryCode; // converting to upper case when shift
-        char code = (char) primaryCode; // converting to char
+
+        char code;
+        if(hebrewMode){
+            code = engToHeb(primaryCode);
+        }
+        else{
+            primaryCode = caps ? primaryCode + ('A' - 'a') : primaryCode; // converting to upper case when shift
+            code = (char) primaryCode; // converting to char
+        }
+
         ic.commitText(String.valueOf(code), 1); //writing to screen
 
         // TODO: 30/12/2020 Add tacking of this key
+    }
+
+    private char engToHeb(int primaryCode){
+        switch (primaryCode){
+            case 'q':
+                return '/';
+            case 'w':
+                return '\'';
+            case 'e':
+                return 'ק';
+            case 'r':
+                return 'ר';
+            case 't':
+                return 'א';
+            case 'y':
+                return 'ט';
+            case 'u':
+                return 'ו';
+            case 'i':
+                return 'ן';
+            case 'o':
+                return 'ם';
+            case 'p':
+                return 'פ';
+            case 'a':
+                return 'ש';
+            case 's':
+                return 'ד';
+            case 'd':
+                return 'ג';
+            case 'f':
+                return 'כ';
+            case 'g':
+                return 'ע';
+            case 'h':
+                return 'י';
+            case 'j':
+                return 'ח';
+            case 'k':
+                return 'ל';
+            case 'l':
+                return 'ך';
+            case 878:
+                return 'ף';
+            case 'z':
+                return 'ז';
+            case 'x':
+                return 'ס';
+            case 'c':
+                return 'ב';
+            case 'v':
+                return 'ה';
+            case 'b':
+                return 'נ';
+            case 'n':
+                return 'מ';
+            case 'm':
+                return 'צ';
+            case 879:
+                return 'ת';
+            case 880:
+                return 'ץ';
+            case 881:
+                return '-';
+
+
+        }
+        return (char)primaryCode; // couldn't find any heb key
     }
 
 
